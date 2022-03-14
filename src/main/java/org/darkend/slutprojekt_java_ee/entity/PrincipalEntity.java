c6e529e51cf4b2da36999c8e2e9d57d94a4fb0be
DarkendHall <marcus.leeman@iths.se>
2022-03-14 10:45:50 +0100

    Add annotation: GeneratedValue

--- src/main/java/org/darkend/slutprojekt_java_ee/entity/PrincipalEntity.java
+++ src/main/java/org/darkend/slutprojekt_java_ee/entity/PrincipalEntity.java
@@ -1,6 +1,8 @@
 package org.darkend.slutprojekt_java_ee.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
@@ -9,6 +11,7 @@ import javax.validation.constraints.Size;
 public class PrincipalEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotEmpty
     @Size(min = 2)
