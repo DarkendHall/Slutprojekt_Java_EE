d043aef2aeced246169c121b6e35931bb3a655f4
ChristianLofqvist <christian.lofqvist@iths.se>
2022-03-15 13:55:13 +0100

    Fix wrong type in PrincipalRepository

--- src/main/java/org/darkend/slutprojekt_java_ee/repository/PrincipalRepository.java
+++ src/main/java/org/darkend/slutprojekt_java_ee/repository/PrincipalRepository.java
@@ -1,9 +1,10 @@
 package org.darkend.slutprojekt_java_ee.repository;
 
 import org.darkend.slutprojekt_java_ee.entity.CourseEntity;
+import org.darkend.slutprojekt_java_ee.entity.PrincipalEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PrincipalRepository extends CrudRepository<CourseEntity, Long> {
+public interface PrincipalRepository extends CrudRepository<PrincipalEntity, Long> {
 }
