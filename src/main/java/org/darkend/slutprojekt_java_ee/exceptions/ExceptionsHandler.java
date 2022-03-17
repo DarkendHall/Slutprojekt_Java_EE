36ebc7824b092aa92bdd689c7ae306dffeb912bf
DarkendHall <marcus.leeman@iths.se>
2022-03-17 10:53:58 +0100

    Update the way exceptions are handled

--- src/main/java/org/darkend/slutprojekt_java_ee/exceptions/ExceptionsHandler.java
+++ src/main/java/org/darkend/slutprojekt_java_ee/exceptions/ExceptionsHandler.java
@@ -1,25 +1,27 @@
 package org.darkend.slutprojekt_java_ee.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.persistence.EntityNotFoundException;
 import javax.validation.ConstraintViolationException;
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class ExceptionsHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ConstraintViolationException handleConstraintViolation(ConstraintViolationException exception) {
-        return exception;
+    public ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException exception) {
+        return ResponseEntity.badRequest()
+                .body(new ExceptionAsJson(LocalDateTime.now()
+                        .toString(), HttpStatus.BAD_REQUEST, exception.getMessage()));
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public EntityNotFoundException handleEntityNotFound(EntityNotFoundException exception) {
-        return exception;
+    public ResponseEntity<Object> handleConstraintViolation(EntityNotFoundException exception) {
+        return new ResponseEntity<>(new ExceptionAsJson(LocalDateTime.now()
+                .toString(), HttpStatus.BAD_REQUEST, exception.getMessage()), HttpStatus.NOT_FOUND);
     }
 }
