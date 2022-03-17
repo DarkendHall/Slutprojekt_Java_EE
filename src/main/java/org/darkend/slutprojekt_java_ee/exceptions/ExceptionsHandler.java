080329481ec9bbc03873f840049793614eaaf605
DarkendHall <marcus.leeman@iths.se>
2022-03-17 11:08:25 +0100

    Add logging for the caught exceptions

--- src/main/java/org/darkend/slutprojekt_java_ee/exceptions/ExceptionsHandler.java
+++ src/main/java/org/darkend/slutprojekt_java_ee/exceptions/ExceptionsHandler.java
@@ -1,5 +1,7 @@
 package org.darkend.slutprojekt_java_ee.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,17 +13,22 @@ import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class ExceptionsHandler {
+    private final Logger logger = LoggerFactory.getLogger(ExceptionsHandler.class);
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException exception) {
+    public ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException e) {
+        logger.warn(e.getMessage());
+
         return ResponseEntity.badRequest()
                 .body(new ExceptionAsJson(LocalDateTime.now()
-                        .toString(), HttpStatus.BAD_REQUEST, exception.getMessage()));
+                        .toString(), HttpStatus.BAD_REQUEST, e.getMessage()));
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<Object> handleConstraintViolation(EntityNotFoundException exception) {
+    public ResponseEntity<Object> handleConstraintViolation(EntityNotFoundException e) {
+        logger.warn(e.getMessage());
+
         return new ResponseEntity<>(new ExceptionAsJson(LocalDateTime.now()
-                .toString(), HttpStatus.BAD_REQUEST, exception.getMessage()), HttpStatus.NOT_FOUND);
+                .toString(), HttpStatus.BAD_REQUEST, e.getMessage()), HttpStatus.NOT_FOUND);
     }
 }
