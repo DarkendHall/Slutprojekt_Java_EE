20a0e68295a8b49033847ee8453365fa8626c1aa
DarkendHall <marcus.leeman@iths.se>
2022-03-17 11:19:16 +0100

    Remove Subject related classes

--- src/main/java/org/darkend/slutprojekt_java_ee/repository/SubjectRepository.java
+++
@@ -1,9 +0,0 @@
-package org.darkend.slutprojekt_java_ee.repository;
-
-import org.darkend.slutprojekt_java_ee.entity.SubjectEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SubjectRepository extends JpaRepository<SubjectEntity, Long> {
-}
