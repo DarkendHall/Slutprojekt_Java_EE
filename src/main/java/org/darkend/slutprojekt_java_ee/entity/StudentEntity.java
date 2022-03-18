bb565e44b837c6fc14ad15fe1ef7af376e86f432
DarkendHall <marcus.leeman@iths.se>
2022-03-18 16:22:43 +0100

    Add method: toString() & Remove associations

--- src/main/java/org/darkend/slutprojekt_java_ee/entity/StudentEntity.java
+++ src/main/java/org/darkend/slutprojekt_java_ee/entity/StudentEntity.java
@@ -1,19 +1,13 @@
 package org.darkend.slutprojekt_java_ee.entity;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 
 @Entity
 public class StudentEntity {
@@ -36,12 +30,6 @@ public class StudentEntity {
     @NotNull
     private String phoneNumber;
 
-    @NotNull
-    @ManyToOne(fetch = FetchType.LAZY)
-    private SchoolEntity school;
-
-    @ManyToMany(mappedBy = "students", cascade = CascadeType.PERSIST)
-    private Set<CourseEntity> courses = new HashSet<>();
 
     public Long getId() {
         return id;
@@ -88,22 +76,15 @@ public class StudentEntity {
         return this;
     }
 
-    public SchoolEntity getSchool() {
-        return school;
-    }
-
-    public StudentEntity setSchool(SchoolEntity school) {
-        this.school = school;
-        return this;
-    }
-
-    public StudentEntity setCourses(Set<CourseEntity> course) {
-        this.courses = course;
-        return this;
-    }
-
-    public Set<CourseEntity> getCourses() {
-        return courses;
+    @Override
+    public String toString() {
+        return "StudentEntity{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
     }
 
     @Override
@@ -113,12 +94,11 @@ public class StudentEntity {
         StudentEntity that = (StudentEntity) o;
         return Objects.equals(id, that.id) && Objects.equals(firstName,
                 that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(email,
-                that.email) && Objects.equals(phoneNumber, that.phoneNumber) && Objects.equals(school,
-                that.school) && Objects.equals(courses, that.courses);
+                that.email) && Objects.equals(phoneNumber, that.phoneNumber);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstName, lastName, email, phoneNumber, school, courses);
+        return Objects.hash(id, firstName, lastName, email, phoneNumber);
     }
 }
