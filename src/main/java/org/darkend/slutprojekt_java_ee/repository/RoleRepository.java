808051e12fbb2b42b46e3a094b82d306fde55480
DarkendHall <marcus.leeman@iths.se>
2022-03-18 16:23:41 +0100

    Add annotation: Transactional

--- src/main/java/org/darkend/slutprojekt_java_ee/repository/RoleRepository.java
+++ src/main/java/org/darkend/slutprojekt_java_ee/repository/RoleRepository.java
@@ -4,7 +4,10 @@ import org.darkend.slutprojekt_java_ee.entity.RoleEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
 public interface RoleRepository extends CrudRepository<RoleEntity, Long> {
+    @Transactional
     RoleEntity findByRole(String role);
 }
