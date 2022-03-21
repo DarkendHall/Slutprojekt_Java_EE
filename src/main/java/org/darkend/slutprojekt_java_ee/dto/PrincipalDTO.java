039164694e22455ca3fdf7d8bba79798340ab123
DarkendHall <marcus.leeman@iths.se>
2022-03-21 10:37:37 +0100

    Update PrincipalDTO to match the entity class

--- src/main/java/org/darkend/slutprojekt_java_ee/dto/PrincipalDTO.java
+++ src/main/java/org/darkend/slutprojekt_java_ee/dto/PrincipalDTO.java
@@ -4,15 +4,25 @@ import java.util.Objects;
 
 public class PrincipalDTO {
 
+    private Long id;
     private String name;
     private String school;
+
+    public Long getId() {
+        return id;
+    }
+
+    public PrincipalDTO setId(Long id) {
+        this.id = id;
+        return this;
+    }
 
     public String getName() {
         return name;
     }
 
-    public PrincipalDTO setName(String firstName, String lastName) {
-        this.name = firstName + " " + lastName;
+    public PrincipalDTO setName(String name) {
+        this.name = name;
         return this;
     }
 
@@ -26,15 +36,25 @@ public class PrincipalDTO {
     }
 
     @Override
+    public String toString() {
+        return "PrincipalDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", school='" + school + '\'' +
+                '}';
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PrincipalDTO that = (PrincipalDTO) o;
-        return Objects.equals(name, that.name) && Objects.equals(school, that.school);
+        return Objects.equals(id, that.id) && Objects.equals(name,
+                that.name) && Objects.equals(school, that.school);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, school);
+        return Objects.hash(id, name, school);
     }
 }
