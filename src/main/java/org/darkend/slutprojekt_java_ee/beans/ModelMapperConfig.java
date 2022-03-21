78e16f46ca5974e7415f6f5d0ce635c747672275
DarkendHall <marcus.leeman@iths.se>
2022-03-21 15:47:03 +0100

    Add ModelMapper configuration & tests

--- src/main/java/org/darkend/slutprojekt_java_ee/beans/ModelMapperConfig.java
+++ src/main/java/org/darkend/slutprojekt_java_ee/beans/ModelMapperConfig.java
@@ -3,8 +3,12 @@ package org.darkend.slutprojekt_java_ee.beans;
 import org.darkend.slutprojekt_java_ee.dto.CommonDTO;
 import org.darkend.slutprojekt_java_ee.dto.DtoToEntityConverter;
 import org.darkend.slutprojekt_java_ee.dto.EntityToDtoConverter;
+import org.darkend.slutprojekt_java_ee.dto.PrincipalDTO;
 import org.darkend.slutprojekt_java_ee.dto.StudentDTO;
+import org.darkend.slutprojekt_java_ee.dto.TeacherDTO;
+import org.darkend.slutprojekt_java_ee.entity.PrincipalEntity;
 import org.darkend.slutprojekt_java_ee.entity.StudentEntity;
+import org.darkend.slutprojekt_java_ee.entity.TeacherEntity;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -19,13 +23,14 @@ public class ModelMapperConfig {
     public ModelMapper modelMapper() {
         ModelMapper mapper = new ModelMapper();
         Student.setUp(mapper);
+        Teacher.setUp(mapper);
+        Principal.setUp(mapper);
         return mapper;
     }
 
     public static ModelMapper getModelMapper() {
         return new ModelMapperConfig().modelMapper();
     }
-
 
     static class Student implements EntityToDtoConverter, DtoToEntityConverter {
         protected static void setUp(ModelMapper mapper) {
@@ -56,6 +61,80 @@ public class ModelMapperConfig {
                             using(ctx -> CommonDTO.generateFirstName(((StudentDTO) ctx.getSource()))).map(source,
                                     destination.getFirstName());
                             using(ctx -> CommonDTO.generateLastName(((StudentDTO) ctx.getSource()))).map(source,
+                                    destination.getLastName());
+                        }
+                    });
+        }
+    }
+
+    static class Teacher implements EntityToDtoConverter, DtoToEntityConverter {
+        protected static void setUp(ModelMapper mapper) {
+            var teacher = new Teacher();
+            teacher.entityToDto(mapper);
+            teacher.dtoToEntity(mapper);
+        }
+
+        @Override
+        public void dtoToEntity(ModelMapper mapper) {
+            mapper.createTypeMap(TeacherEntity.class, TeacherDTO.class)
+                    .addMappings(
+                            new PropertyMap<>() {
+                                @Override
+                                protected void configure() {
+                                    using(ctx -> CommonDTO.generateFullName(
+                                            ((TeacherEntity) ctx.getSource()).getFirstName(),
+                                            ((TeacherEntity) ctx.getSource()).getLastName()
+                                    )).map(source, destination.getFullName());
+                                }
+                            });
+        }
+
+        @Override
+        public void entityToDto(ModelMapper mapper) {
+            mapper.createTypeMap(TeacherDTO.class, TeacherEntity.class)
+                    .addMappings(new PropertyMap<>() {
+                        @Override
+                        protected void configure() {
+                            using(ctx -> CommonDTO.generateFirstName(((TeacherDTO) ctx.getSource()))).map(source,
+                                    destination.getFirstName());
+                            using(ctx -> CommonDTO.generateLastName(((TeacherDTO) ctx.getSource()))).map(source,
+                                    destination.getLastName());
+                        }
+                    });
+        }
+    }
+
+    private static class Principal implements EntityToDtoConverter, DtoToEntityConverter {
+        protected static void setUp(ModelMapper mapper) {
+            var principal = new Principal();
+            principal.entityToDto(mapper);
+            principal.dtoToEntity(mapper);
+        }
+
+        @Override
+        public void dtoToEntity(ModelMapper mapper) {
+            mapper.createTypeMap(PrincipalEntity.class, PrincipalDTO.class)
+                    .addMappings(
+                            new PropertyMap<>() {
+                                @Override
+                                protected void configure() {
+                                    using(ctx -> CommonDTO.generateFullName(
+                                            ((PrincipalEntity) ctx.getSource()).getFirstName(),
+                                            ((PrincipalEntity) ctx.getSource()).getLastName()
+                                    )).map(source, destination.getFullName());
+                                }
+                            });
+        }
+
+        @Override
+        public void entityToDto(ModelMapper mapper) {
+            mapper.createTypeMap(PrincipalDTO.class, PrincipalEntity.class)
+                    .addMappings(new PropertyMap<>() {
+                        @Override
+                        protected void configure() {
+                            using(ctx -> CommonDTO.generateFirstName(((PrincipalDTO) ctx.getSource()))).map(source,
+                                    destination.getFirstName());
+                            using(ctx -> CommonDTO.generateLastName(((PrincipalDTO) ctx.getSource()))).map(source,
                                     destination.getLastName());
                         }
                     });
--- src/test/java/org/darkend/slutprojekt_java_ee/dto/ModelMapperConfigTest.java
+++ src/test/java/org/darkend/slutprojekt_java_ee/dto/ModelMapperConfigTest.java
@@ -1,11 +1,17 @@
 package org.darkend.slutprojekt_java_ee.dto;
 
 import org.darkend.slutprojekt_java_ee.beans.ModelMapperConfig;
+import org.darkend.slutprojekt_java_ee.entity.CourseEntity;
+import org.darkend.slutprojekt_java_ee.entity.PrincipalEntity;
+import org.darkend.slutprojekt_java_ee.entity.SchoolEntity;
 import org.darkend.slutprojekt_java_ee.entity.StudentEntity;
+import org.darkend.slutprojekt_java_ee.entity.TeacherEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
+
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -14,85 +20,145 @@ class ModelMapperConfigTest {
     private final ModelMapper mapper = ModelMapperConfig.getModelMapper();
 
     @Nested
-    class StudentTests {
+    class SchoolEntityToDTOTests {
+        private SchoolEntity school;
 
-        @Nested
-        class StudentEntityToDTOTests {
+        @BeforeEach
+        public void setUp() {
+            TeacherEntity teacher = new TeacherEntity().setId(1L)
+                    .setFirstName("First")
+                    .setLastName("Last");
 
-            private StudentEntity entity;
+            StudentEntity student = new StudentEntity().setId(1L)
+                    .setFirstName("First")
+                    .setLastName("Last")
+                    .setEmail("email@email.com")
+                    .setPhoneNumber("N/A");
 
-            @BeforeEach
-            public void setUp() {
-                entity = new StudentEntity().setId(1L)
-                        .setFirstName("First")
-                        .setLastName("Last")
-                        .setPhoneNumber("N/A")
-                        .setEmail("email@email.com");
+            CourseEntity course = new CourseEntity().setId(1L)
+                    .setName("Name")
+                    .setStudents(Set.of(student))
+                    .setTeacher(teacher);
 
-            }
+            school = new SchoolEntity().setId(1L)
+                    .setName("Name")
+                    .setStudents(Set.of(student))
+                    .setTeachers(Set.of(teacher))
+                    .setCourses(Set.of(course))
+                    .setAddress("Address")
+                    .setCity("City");
 
-            @Test
-            void firstNameAndLastNameShouldBeCombinedToFullName() {
-                var result = mapper.map(entity, StudentDTO.class);
+            PrincipalEntity principal = new PrincipalEntity().setId(1L)
+                    .setFirstName("First")
+                    .setLastName("Last");
 
-                assertThat(result.getFullName()).isEqualTo("First Last");
-            }
+            school.setPrincipal(principal);
+        }
 
+        @Test
+        void fullMappingShouldBeTheEqualToManualDTO() {
+            StudentDTO studentDto = new StudentDTO().setId(1L)
+                    .setFullName("First Last")
+                    .setEmail("email@email.com")
+                    .setPhoneNumber("N/A");
 
-            @Test
-            void fullMappingShouldBeTheEqualToManualDTO() {
-                var dto = new StudentDTO().setId(1L)
-                        .setFullName("First Last")
-                        .setPhoneNumber("N/A")
-                        .setEmail("email@email.com");
+            TeacherDTO teacherDto = new TeacherDTO().setId(1L)
+                    .setFullName("First Last");
 
-                var result = mapper.map(entity, StudentDTO.class);
+            CourseDTO courseDto = new CourseDTO().setId(1L)
+                    .setName("Name")
+                    .setStudents(Set.of(studentDto))
+                    .setTeacher(teacherDto);
 
-                assertThat(result.getId()).isEqualTo(1L);
-                assertThat(result.getFullName()).isEqualTo("First Last");
-                assertThat(result.getPhoneNumber()).isEqualTo("N/A");
-                assertThat(result.getEmail()).isEqualTo("email@email.com");
-                assertThat(result).isEqualTo(dto);
-            }
+            SchoolDTO schoolDto = new SchoolDTO().setId(1L)
+                    .setName("Name")
+                    .setCity("City")
+                    .setAddress("Address")
+                    .setStudents(Set.of(studentDto))
+                    .setTeachers(Set.of(teacherDto))
+                    .setCourses(Set.of(courseDto));
+
+            PrincipalDTO principalDto = new PrincipalDTO().setId(1L)
+                    .setFullName("First Last");
+            schoolDto.setPrincipal(principalDto);
+
+            var result = mapper.map(school, SchoolDTO.class);
+
+            assertThat(result).isEqualTo(schoolDto);
+        }
+
+    }
+
+    @Nested
+    class SchoolDTOToEntityTests {
+
+        private SchoolDTO schoolDto;
+
+        @BeforeEach
+        public void setUp() {
+            StudentDTO studentDto = new StudentDTO().setId(1L)
+                    .setFullName("First Last")
+                    .setEmail("email@email.com")
+                    .setPhoneNumber("N/A");
+
+            TeacherDTO teacherDto = new TeacherDTO().setId(1L)
+                    .setFullName("First Last");
+
+            CourseDTO courseDto = new CourseDTO().setId(1L)
+                    .setName("Name")
+                    .setStudents(Set.of(studentDto))
+                    .setTeacher(teacherDto);
+
+            schoolDto = new SchoolDTO().setId(1L)
+                    .setName("Name")
+                    .setCity("City")
+                    .setAddress("Address")
+                    .setStudents(Set.of(studentDto))
+                    .setTeachers(Set.of(teacherDto))
+                    .setCourses(Set.of(courseDto));
+
+            PrincipalDTO principalDto = new PrincipalDTO().setId(1L)
+                    .setFullName("First Last");
+
+            schoolDto.setPrincipal(principalDto);
+        }
+
+        @Test
+        void fullMappingShouldBeTheEqualToManualEntity() {
+            TeacherEntity teacher = new TeacherEntity().setId(1L)
+                    .setFirstName("First")
+                    .setLastName("Last");
+
+            StudentEntity student = new StudentEntity().setId(1L)
+                    .setFirstName("First")
+                    .setLastName("Last")
+                    .setEmail("email@email.com")
+                    .setPhoneNumber("N/A");
+
+            CourseEntity course = new CourseEntity().setId(1L)
+                    .setName("Name")
+                    .setStudents(Set.of(student))
+                    .setTeacher(teacher);
+
+            SchoolEntity school = new SchoolEntity().setId(1L)
+                    .setName("Name")
+                    .setStudents(Set.of(student))
+                    .setTeachers(Set.of(teacher))
+                    .setCourses(Set.of(course))
+                    .setAddress("Address")
+                    .setCity("City");
+
+            PrincipalEntity principal = new PrincipalEntity().setId(1L)
+                    .setFirstName("First")
+                    .setLastName("Last");
+
+            school.setPrincipal(principal);
+
+            var result = mapper.map(schoolDto, SchoolEntity.class);
+
+            assertThat(result).isEqualTo(school);
 
         }
 
-        @Nested
-        class StudentDTOToEntityTests {
-
-            private StudentDTO dto;
-
-            @BeforeEach
-            public void setUp() {
-                dto = new StudentDTO().setId(1L)
-                        .setFullName("First Last")
-                        .setPhoneNumber("N/A")
-                        .setEmail("email@email.com");
-            }
-
-            @Test
-            void fullNameShouldBeSplitIntoFirstNameAndLastName() {
-                var result = mapper.map(dto, StudentEntity.class);
-
-                assertThat(result.getFirstName() + " " + result.getLastName()).isEqualTo("First Last");
-            }
-
-            @Test
-            void fullMappingShouldBeTheEqualToManualEntity() {
-                StudentEntity entity = new StudentEntity().setId(1L)
-                        .setFirstName("First")
-                        .setLastName("Last")
-                        .setPhoneNumber("N/A")
-                        .setEmail("email@email.com");
-                var result = mapper.map(dto, StudentEntity.class);
-
-                assertThat(result.getId()).isEqualTo(1L);
-                assertThat(result.getFirstName()).isEqualTo("First");
-                assertThat(result.getLastName()).isEqualTo("Last");
-                assertThat(result.getPhoneNumber()).isEqualTo("N/A");
-                assertThat(result.getEmail()).isEqualTo("email@email.com");
-                assertThat(result).isEqualTo(entity);
-            }
-        }
     }
 }
