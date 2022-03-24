262047ac5d67e02403101fe2fa85caaa2a1b02a2
DarkendHall <marcus.leeman@iths.se>
2022-03-24 15:49:10 +0100

    Add class: CourseDtoTest

---
+++ src/test/java/org/darkend/slutprojekt_java_ee/dto/CourseDtoTest.java
@@ -0,0 +1,65 @@
+package org.darkend.slutprojekt_java_ee.dto;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CourseDtoTest {
+
+    private CourseDto course;
+
+    @BeforeEach
+    void setUp() {
+        course = new CourseDto();
+    }
+
+    @Test
+    void getId() {
+        course.setId(1L);
+
+        assertThat(course.getId()).isEqualTo(1L);
+    }
+
+    @Test
+    void getName() {
+        course.setName("Name");
+
+        assertThat(course.getName()).isEqualTo("Name");
+    }
+
+    @Test
+    void getStudents() {
+        StudentDto student = new StudentDto();
+
+        course.setStudents(List.of(student));
+
+        assertThat(course.getStudents()).isEqualTo(List.of(student));
+    }
+
+    @Test
+    void getTeacher() {
+        TeacherDto teacher = new TeacherDto();
+
+        course.setTeacher(teacher);
+
+        assertThat(course.getTeacher()).isEqualTo(teacher);
+    }
+
+    @Test
+    void testToString() {
+        assertThat(course.toString()).isEqualTo("CourseDto{id=null, name='null', students=null, teacher=null}");
+    }
+
+    @Test
+    void testEquals() {
+        assertThat(course).isEqualTo(new CourseDto());
+    }
+
+    @Test
+    void testHashCode() {
+        assertThat(course.hashCode()).isEqualTo(new CourseDto().hashCode());
+    }
+}
