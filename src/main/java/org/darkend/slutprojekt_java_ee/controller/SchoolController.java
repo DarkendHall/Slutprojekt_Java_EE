6ab915862b829d6f620d486fed9e896a5199b045
DarkendHall <marcus.leeman@iths.se>
2022-03-28 15:07:12 +0200

    Update Controller classes to be annotation based
    Update Controller classes to use annotations rather than
    ResponseEntities because the response status defaults to 200 OK when
    using OpenAPI/Swagger2.

--- src/main/java/org/darkend/slutprojekt_java_ee/controller/SchoolController.java
+++ src/main/java/org/darkend/slutprojekt_java_ee/controller/SchoolController.java
@@ -1,20 +1,24 @@
 package org.darkend.slutprojekt_java_ee.controller;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.darkend.slutprojekt_java_ee.dto.ObjectToJson;
 import org.darkend.slutprojekt_java_ee.dto.SchoolDto;
 import org.darkend.slutprojekt_java_ee.service.SchoolService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -29,32 +33,39 @@ public class SchoolController {
     }
 
     @PostMapping()
-    public ResponseEntity<SchoolDto> createSchool(@RequestBody SchoolDto school) {
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiResponses({@ApiResponse(responseCode = "404", description = "Not found"), @ApiResponse(responseCode = "401",
+            description = "Unauthorized"), @ApiResponse(responseCode = "400",
+            description = "Bad Request"), @ApiResponse(responseCode = "403", description = "Forbidden")})
+    public SchoolDto createSchool(@RequestBody SchoolDto school, HttpServletResponse response) {
         logger.info(String.format("Received POST request with JSON body: %s", ObjectToJson.convert(school)));
         SchoolDto createdSchool = schoolService.createSchool(school);
-        return ResponseEntity.created(URI.create("/schools/" + createdSchool.getId()))
-                .body(createdSchool);
+        response.addHeader("Location", ServletUriComponentsBuilder.fromCurrentRequest()
+                .build() + "/" + createdSchool.getId());
+        return createdSchool;
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<Void> deleteSchool(@PathVariable Long id) {
-        logger.info(String.format("Received DELETE request with ID: %d", id));
+    @ApiResponses({@ApiResponse(responseCode = "404", description = "Not found"), @ApiResponse(responseCode = "401",
+            description = "Unauthorized"), @ApiResponse(responseCode = "403", description = "Forbidden")})
+    public void deleteSchool(@PathVariable Long id) {
+        logger.info(String.format("Received DELETE request for ID: %d", id));
         schoolService.deleteSchool(id);
-        return ResponseEntity.ok()
-                .build();
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<SchoolDto> findSchoolById(@PathVariable Long id) {
-        logger.info(String.format("Received GET request with ID: %d", id));
-        SchoolDto foundSchool = schoolService.findSchoolById(id);
-        return ResponseEntity.ok(foundSchool);
+    @ApiResponses({@ApiResponse(responseCode = "404", description = "Not found"), @ApiResponse(responseCode = "401",
+            description = "Unauthorized")})
+    public SchoolDto findSchoolById(@PathVariable Long id) {
+        logger.info(String.format("Received GET request for ID: %d", id));
+        return schoolService.findSchoolById(id);
     }
 
     @GetMapping()
-    public ResponseEntity<List<SchoolDto>> findAllSchools() {
+    @ApiResponses({@ApiResponse(responseCode = "404", description = "Not found"), @ApiResponse(responseCode = "401",
+            description = "Unauthorized")})
+    public List<SchoolDto> findAllSchools() {
         logger.info("Received GET request for all schools");
-        List<SchoolDto> allSchools = schoolService.findAllSchools();
-        return ResponseEntity.ok(allSchools);
+        return schoolService.findAllSchools();
     }
 }
