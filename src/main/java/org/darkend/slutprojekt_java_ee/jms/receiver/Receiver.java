70a672d1ad5fb757432eac9e72e46dc7345b09cc
Oscar Stjernfeldt <oscarstjernfeldt@hotmail.com>
2022-03-29 17:28:40 +0200

    Update class: Receiver

--- src/main/java/org/darkend/slutprojekt_java_ee/jms/receiver/Receiver.java
+++ src/main/java/org/darkend/slutprojekt_java_ee/jms/receiver/Receiver.java
@@ -1,7 +1,11 @@
 package org.darkend.slutprojekt_java_ee.jms.receiver;
 
+import com.mailjet.client.errors.MailjetException;
 import org.darkend.slutprojekt_java_ee.entity.MailEntity;
+import org.darkend.slutprojekt_java_ee.entity.RecipientEntity;
 import org.darkend.slutprojekt_java_ee.jms.confiq.JmsConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -9,8 +13,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class Receiver {
 
+    private final Logger logger = LoggerFactory.getLogger(Receiver.class);
+
+    private final EmailSender emailSender = new EmailSender();
+
     @JmsListener(destination = JmsConfig.QUEUE)
     public void listener(@Payload MailEntity message) {
-        System.out.println(message.getMsg());
+
+        logger.info(String.format("Received message: %s", message));
+
+        var recipients = message.getRecipients()
+                .stream()
+                .map(RecipientEntity::getEmail)
+                .toList();
+        try {
+            var response = emailSender.sendMail(message.getMsg(), recipients);
+            logger.info(String.format("Email was sent with data %s", response.getData()));
+        } catch (MailjetException e) {
+            logger.warn(String.format("Failed to send message because: %s", e));
+        }
     }
 }
