e3fb460f5f408e30f8696debbf8d360cffa1a62d
Oscar Stjernfeldt <oscarstjernfeldt@hotmail.com>
2022-03-29 13:20:04 +0200

    Add class: MailController

---
+++ src/main/java/org/darkend/slutprojekt_java_ee/controller/MailController.java
@@ -0,0 +1,38 @@
+package org.darkend.slutprojekt_java_ee.controller;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.darkend.slutprojekt_java_ee.dto.MailDto;
+import org.darkend.slutprojekt_java_ee.dto.ObjectToJson;
+import org.darkend.slutprojekt_java_ee.service.MailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MailController {
+
+    private final MailService mailService;
+    private final Logger logger = LoggerFactory.getLogger(MailController.class);
+
+    public MailController(MailService mailService) {
+        this.mailService = mailService;
+    }
+
+
+    @PostMapping("courses/{id}/message")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiResponses({@ApiResponse(responseCode = "404", description = "Not found"), @ApiResponse(responseCode = "401",
+            description = "Unauthorized"), @ApiResponse(responseCode = "400",
+            description = "Bad Request"), @ApiResponse(responseCode = "403", description = "Forbidden")})
+    public MailDto sendEmail(@RequestBody MailDto mail, @PathVariable Long id) {
+        logger.info(String.format("Received POST request with JSON body: %s", ObjectToJson.convert(mail)));
+
+        return mailService.createMail(mail, id);
+    }
+}
