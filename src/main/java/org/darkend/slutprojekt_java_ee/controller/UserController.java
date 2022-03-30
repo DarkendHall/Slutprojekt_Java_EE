d78690bfad7ee7e008d367dcdd15eb484468a558
OscarStjernfeldt <oscarstjernfeldt@hotmail.com>
2022-03-30 11:23:27 +0200

    Add class: UserController

---
+++ src/main/java/org/darkend/slutprojekt_java_ee/controller/UserController.java
@@ -0,0 +1,43 @@
+package org.darkend.slutprojekt_java_ee.controller;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.darkend.slutprojekt_java_ee.dto.ObjectToJson;
+import org.darkend.slutprojekt_java_ee.dto.UserDtoIn;
+import org.darkend.slutprojekt_java_ee.dto.UserDtoOut;
+import org.darkend.slutprojekt_java_ee.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+    private final UserService userService;
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("signup")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiResponses({@ApiResponse(responseCode = "401", description = "Unauthorized"), @ApiResponse(responseCode = "400",
+            description = "Bad Request"), @ApiResponse(responseCode = "403", description = "Forbidden")})
+    public UserDtoOut createUser(@RequestBody UserDtoIn user, HttpServletResponse response) {
+        logger.info(String.format("Received POST request with JSON body: %s", ObjectToJson.convert(user)));
+        UserDtoOut createdUser = userService.createUser(user);
+        response.addHeader("Location", ServletUriComponentsBuilder.fromCurrentRequest()
+                .build() + "/" + createdUser.getId());
+        return createdUser;
+    }
+}
