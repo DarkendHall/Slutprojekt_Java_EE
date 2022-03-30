8b753e776bc621a7dced83ee94affdd46a89dead
DarkendHall <marcus.leeman@iths.se>
2022-03-30 12:39:19 +0200

    Add constraints to CourseDto

--- src/main/java/org/darkend/slutprojekt_java_ee/dto/CourseDto.java
+++ src/main/java/org/darkend/slutprojekt_java_ee/dto/CourseDto.java
@@ -2,15 +2,27 @@ package org.darkend.slutprojekt_java_ee.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Size;
 import java.util.List;
 import java.util.Objects;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CourseDto {
 
+    @Null
     private Long id;
+
+    @NotEmpty
+    @Size(min = 2)
     private String name;
+
+    @NotEmpty
     private List<StudentDto> students;
+
+    @NotNull
     private TeacherDto teacher;
 
     public Long getId() {
