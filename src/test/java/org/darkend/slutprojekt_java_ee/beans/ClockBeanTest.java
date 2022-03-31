241bee946e07ef7b055689c0dbec7180d8350e64
DarkendHall <marcus.leeman@iths.se>
2022-03-31 17:26:45 +0200

    Add test: clockBeanIsInstanceOfClock

---
+++ src/test/java/org/darkend/slutprojekt_java_ee/beans/ClockBeanTest.java
@@ -0,0 +1,20 @@
+package org.darkend.slutprojekt_java_ee.beans;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ClockBeanTest {
+
+    private final ClockBean clockBean = new ClockBean();
+
+    @Test
+    void clockBeanIsInstanceOfClock() {
+        var result = clockBean.clock();
+
+        assertThat(result).isInstanceOf(Clock.class);
+    }
+
+}
