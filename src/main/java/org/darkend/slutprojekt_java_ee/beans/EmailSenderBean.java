461564fe3d3c5c754b2db16c057560f2c1a54a3c
OscarStjernfeldt <oscarstjernfeldt@hotmail.com>
2022-04-01 15:46:12 +0200

    Add class: EmailSenderBean

---
+++ src/main/java/org/darkend/slutprojekt_java_ee/beans/EmailSenderBean.java
@@ -0,0 +1,16 @@
+package org.darkend.slutprojekt_java_ee.beans;
+
+import com.mailjet.client.MailjetClient;
+import org.darkend.slutprojekt_java_ee.jms.receiver.EmailSender;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmailSenderBean {
+
+    @Bean
+    public EmailSender emailSender(MailjetClient client) {
+        return new EmailSender(client);
+    }
+
+}
