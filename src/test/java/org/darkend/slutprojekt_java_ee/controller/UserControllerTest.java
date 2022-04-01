197a638d7c2aed6a3b043ca5a8dc9e42d27b1df2
OscarStjernfeldt <oscarstjernfeldt@hotmail.com>
2022-04-01 10:11:30 +0200

    Add class: UserControllerTest

---
+++ src/test/java/org/darkend/slutprojekt_java_ee/controller/UserControllerTest.java
@@ -0,0 +1,83 @@
+package org.darkend.slutprojekt_java_ee.controller;
+
+import org.darkend.slutprojekt_java_ee.dto.UserDtoIn;
+import org.darkend.slutprojekt_java_ee.dto.UserDtoOut;
+import org.darkend.slutprojekt_java_ee.security.SecurityConfig;
+import org.darkend.slutprojekt_java_ee.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.Clock;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+@Import({ModelMapper.class, SecurityConfig.class})
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    private UserService service;
+
+    private final UserDtoIn userIn = new UserDtoIn().setId(2L)
+            .setUsername("user")
+            .setPassword("password")
+            .setRoles(List.of("ROLE_USER"));
+
+    private final UserDtoOut userOut = new UserDtoOut().setId(2L)
+            .setUsername("user")
+            .setRoles(List.of("ROLE_USER"));
+
+    @BeforeEach
+    void setUp() {
+        when(service.createUser(any())).thenReturn(userOut);
+    }
+
+    @Test
+    public void addNewUserWithPostReturnsCreatedUser() throws Exception {
+        mvc.perform(post("/users/signup").contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                  "id": 2,
+                                  "username": "user",
+                                  "password": "password",
+                                  "roles": [
+                                  	"ROLE_USER"
+                                  ]
+                                }
+                                """))
+                .andDo(System.out::println)
+                .andExpect(jsonPath("$.id").value(userIn.getId()))
+                .andExpect(jsonPath("$.username").value(userIn.getUsername()))
+                .andExpect(jsonPath("$.roles[0]").value(userIn.getRoles()
+                        .get(0)))
+                .andExpect(status().isCreated());
+    }
+
+    @TestConfiguration
+    static class TestConfig {
+
+        @Bean
+        public Clock clock() {
+            return Clock.systemDefaultZone();
+        }
+    }
+}
