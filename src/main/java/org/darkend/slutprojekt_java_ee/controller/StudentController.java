6c16e6aa143179dadb85bd2648d7b0a33633f55b
ChristianLofqvist <christian.lofqvist@iths.se>
2022-04-04 16:47:18 +0200

    Add update functionality

--- src/main/java/org/darkend/slutprojekt_java_ee/controller/StudentController.java
+++ src/main/java/org/darkend/slutprojekt_java_ee/controller/StudentController.java
@@ -3,6 +3,8 @@ package org.darkend.slutprojekt_java_ee.controller;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.darkend.slutprojekt_java_ee.dto.ObjectToJson;
+import org.darkend.slutprojekt_java_ee.dto.PrincipalDto;
+import org.darkend.slutprojekt_java_ee.dto.SchoolDto;
 import org.darkend.slutprojekt_java_ee.dto.StudentDto;
 import org.darkend.slutprojekt_java_ee.service.StudentService;
 import org.slf4j.Logger;
@@ -10,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.Digits;
 import java.util.List;
 
 @RestController
@@ -67,5 +71,24 @@ public class StudentController {
     public List<StudentDto> findAllStudents() {
         logger.info("Received GET request for all students");
         return studentService.findAllStudents();
+    }
+
+
+    @PatchMapping("{id}/email")
+    @ApiResponses({@ApiResponse(responseCode = "404", description = "Not found"), @ApiResponse(responseCode = "401",
+            description = "Unauthorized"), @ApiResponse(responseCode = "400",
+            description = "Bad Request"), @ApiResponse(responseCode = "403", description = "Forbidden")})
+    public StudentDto setEmailInStudent(@PathVariable Long id, @RequestBody String email) {
+        logger.info("Received PATCH request with JSON body: {}", email);
+        return studentService.updateEmail(email, id);
+    }
+
+    @PatchMapping("{id}/phonenumber")
+    @ApiResponses({@ApiResponse(responseCode = "404", description = "Not found"), @ApiResponse(responseCode = "401",
+            description = "Unauthorized"), @ApiResponse(responseCode = "400",
+            description = "Bad Request"), @ApiResponse(responseCode = "403", description = "Forbidden")})
+    public StudentDto setPhoneNumberInStudent(@PathVariable Long id, @RequestBody String phoneNumber) {
+        logger.info("Received PATCH request with JSON body: {}", phoneNumber);
+        return studentService.updatePhoneNumber(phoneNumber, id);
     }
 }
--- src/main/java/org/darkend/slutprojekt_java_ee/service/StudentService.java
+++ src/main/java/org/darkend/slutprojekt_java_ee/service/StudentService.java
@@ -1,10 +1,20 @@
 package org.darkend.slutprojekt_java_ee.service;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.darkend.slutprojekt_java_ee.dto.CourseDto;
+import org.darkend.slutprojekt_java_ee.dto.ObjectToJson;
+import org.darkend.slutprojekt_java_ee.dto.PrincipalDto;
+import org.darkend.slutprojekt_java_ee.dto.SchoolDto;
 import org.darkend.slutprojekt_java_ee.dto.StudentDto;
+import org.darkend.slutprojekt_java_ee.entity.PrincipalEntity;
 import org.darkend.slutprojekt_java_ee.entity.StudentEntity;
 import org.darkend.slutprojekt_java_ee.repository.StudentRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
@@ -40,5 +50,21 @@ public class StudentService {
                 .stream()
                 .map(student -> mapper.map(student, StudentDto.class))
                 .toList();
+    }
+
+    public StudentDto updateEmail(String email, Long id) {
+        var studentEntity = studentRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("No student found with ID: " + id));
+        studentEntity.setEmail(email);
+        var savedEntity = studentRepository.save(studentEntity);
+        return mapper.map(savedEntity, StudentDto.class);
+    }
+
+    public StudentDto updatePhoneNumber(String phoneNumber, Long id) {
+        var studentEntity = studentRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("No student found with ID: " + id));
+        studentEntity.setPhoneNumber(phoneNumber);
+        var savedEntity = studentRepository.save(studentEntity);
+        return mapper.map(savedEntity, StudentDto.class);
     }
 }
