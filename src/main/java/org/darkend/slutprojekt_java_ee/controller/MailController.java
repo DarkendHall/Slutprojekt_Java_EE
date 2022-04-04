22ba5c47ab444f87027bd55f75681926d1aaba7a
DarkendHall <marcus.leeman@iths.se>
2022-04-04 15:41:41 +0200

    Refactor code

--- src/main/java/org/darkend/slutprojekt_java_ee/controller/MailController.java
+++ src/main/java/org/darkend/slutprojekt_java_ee/controller/MailController.java
@@ -1,7 +1,6 @@
 package org.darkend.slutprojekt_java_ee.controller;
 
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.darkend.slutprojekt_java_ee.dto.MailDto;
 import org.darkend.slutprojekt_java_ee.dto.ObjectToJson;
 import org.darkend.slutprojekt_java_ee.service.MailService;
@@ -26,11 +25,13 @@ public class MailController {
 
     @PostMapping("courses/{id}/message")
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiResponses({@ApiResponse(responseCode = "404", description = "Not found"), @ApiResponse(responseCode = "401",
-            description = "Unauthorized"), @ApiResponse(responseCode = "400",
-            description = "Bad Request"), @ApiResponse(responseCode = "403", description = "Forbidden")})
+    @ApiResponse(responseCode = "400", description = "Bad Request")
+    @ApiResponse(responseCode = "401", description = "Unauthorized")
+    @ApiResponse(responseCode = "404", description = "Not found")
+    @ApiResponse(responseCode = "403", description = "Forbidden")
     public MailDto sendEmail(@RequestBody MailDto mail, @PathVariable Long id) {
-        logger.info("Received POST request with JSON body: {}", ObjectToJson.convert(mail));
+        String jsonBody = ObjectToJson.convert(mail);
+        logger.info("Received POST request with JSON body: {}", jsonBody);
 
         return mailService.newMail(mail, id);
     }
