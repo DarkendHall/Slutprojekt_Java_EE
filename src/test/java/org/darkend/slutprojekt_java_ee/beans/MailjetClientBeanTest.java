f69f12fdf02e8a8516746181ab19ca8547209f1f
DarkendHall <marcus.leeman@iths.se>
2022-04-04 15:06:00 +0200

    Add class: MailjetClientBeanTest

---
+++ src/test/java/org/darkend/slutprojekt_java_ee/beans/MailjetClientBeanTest.java
@@ -0,0 +1,17 @@
+package org.darkend.slutprojekt_java_ee.beans;
+
+import com.mailjet.client.MailjetClient;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MailjetClientBeanTest {
+
+    private final MailjetClientBean mailjetClientBean = new MailjetClientBean();
+
+    @Test
+    void mailjetClientShouldReturnMailjetClient() {
+        assertThat(mailjetClientBean.mailjetClient()).isExactlyInstanceOf(MailjetClient.class);
+    }
+
+}
