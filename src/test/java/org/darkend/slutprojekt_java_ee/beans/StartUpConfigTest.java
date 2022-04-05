0ca0ac82d1f0c5820b16d16307f32d0652f59f80
Marcus Leeman <89127123+DarkendHall@users.noreply.github.com>
2022-04-05 10:19:02 +0200

    Refactor code
    * Refactor record & tests for ExceptionAsJson
    
    * Fix exclusion of Application class
    
    * Add blank line
    
    * Add another test to test all scenarios
    
    * Refactor equals and hashCode test

--- src/test/java/org/darkend/slutprojekt_java_ee/beans/StartUpConfigTest.java
+++ src/test/java/org/darkend/slutprojekt_java_ee/beans/StartUpConfigTest.java
@@ -1,20 +1,43 @@
 package org.darkend.slutprojekt_java_ee.beans;
 
+import org.darkend.slutprojekt_java_ee.entity.RoleEntity;
 import org.darkend.slutprojekt_java_ee.repository.RoleRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class StartUpConfigTest {
 
-    private final StartUpConfig startUpConfig = new StartUpConfig();
+    private StartUpConfig startUpConfig;
+    private RoleRepository roleRepository;
 
-    private final RoleRepository roleRepository = mock(RoleRepository.class);
+    @BeforeEach
+    void setUp() {
+        startUpConfig = new StartUpConfig();
+        roleRepository = mock(RoleRepository.class);
+    }
 
     @Test
-    void testCommandLineRunner() throws Exception {
+    void testCommandLineRunnerWithNull() throws Exception {
+        var clr = startUpConfig.setUpRoles(roleRepository);
+
+        clr.run();
+
+        verify(roleRepository, times(1)).findByRole("ROLE_ADMIN");
+        verify(roleRepository, times(1)).findByRole("ROLE_USER");
+    }
+
+    @Test
+    void testCommandLineRunnerWithNonNull() throws Exception {
+        when(roleRepository.findByRole("ROLE_USER")).thenReturn(new RoleEntity().setRole("ROLE_USER")
+                .setId(1L));
+        when(roleRepository.findByRole("ROLE_ADMIN")).thenReturn(new RoleEntity().setRole("ROLE_ADMIN")
+                .setId(2L));
+
         var clr = startUpConfig.setUpRoles(roleRepository);
 
         clr.run();
--- src/test/java/org/darkend/slutprojekt_java_ee/exceptions/ExceptionAsJsonTest.java
+++ src/test/java/org/darkend/slutprojekt_java_ee/exceptions/ExceptionAsJsonTest.java
@@ -1,5 +1,6 @@
 package org.darkend.slutprojekt_java_ee.exceptions;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
@@ -10,32 +11,29 @@ class ExceptionAsJsonTest {
     ExceptionAsJson json = new ExceptionAsJson("time", HttpStatus.NOT_FOUND, "msg");
 
     @Test
-    void getTimestamp() {
-        assertThat(json.getTimestamp()).isEqualTo("time");
+    void timestamp() {
+        assertThat(json.timestamp()).isEqualTo("time");
     }
 
     @Test
-    void getStatus() {
-        assertThat(json.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+    void status() {
+        assertThat(json.status()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
     @Test
-    void getMessage() {
-        assertThat(json.getMessage()).isEqualTo("msg");
+    void message() {
+        assertThat(json.message()).isEqualTo("msg");
     }
 
     @Test
     void testToString() {
-        assertThat(json).hasToString("ExceptionAsJson{timestamp='time', status=404 NOT_FOUND, message='msg'}");
+        assertThat(json).hasToString("ExceptionAsJson[timestamp=time, status=404 NOT_FOUND, message=msg]");
     }
 
     @Test
     void testEquals() {
-        assertThat(json).isEqualTo(new ExceptionAsJson("time", HttpStatus.NOT_FOUND, "msg"));
-    }
-
-    @Test
-    void testHashCode() {
-        assertThat(json).hasSameHashCodeAs(new ExceptionAsJson("time", HttpStatus.NOT_FOUND, "msg"));
+        EqualsVerifier.simple()
+                .forClass(ExceptionAsJson.class)
+                .verify();
     }
 }
